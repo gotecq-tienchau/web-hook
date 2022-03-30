b11f10ea411c7a72ef93ffe7b52c92462a72bdfa
tiencv <tiencv@fiisoft.com>
2022-03-30 21:41:50 +0700

    update: remove logs

--- src/controllers/page/web-hook.js
+++ src/controllers/page/web-hook.js
@@ -1,12 +1,7 @@
 const HookModel = require('../../models/hooks');
-
 class WebHookController {
     GetIndex = async (req, res) => {
         const data = await HookModel.find();
-        console.log(
-            '🚀 ~ file: web-hook.js ~ line 6 ~ WebHookController ~ GetIndex= ~ data',
-            data
-        );
         res.status(200).render('web-hook/home', { data });
     };
 }
