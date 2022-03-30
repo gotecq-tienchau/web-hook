f95744849366af2b052d742dc45049ea2e59bd81
tiencv <tiencv@fiisoft.com>
2022-03-30 15:26:19 +0700

    feat: logs

--- src/controllers/web-hook.js
+++ src/controllers/web-hook.js
@@ -1,18 +1,20 @@
 class WebHookController {
     GetIndex = (req, res) => {
-        const { body, query, params } = req;
-        console.log(
-            '🚀 ~ file: web-hook.js ~ line 4 ~ WebHookController ~ { body, query, params }',
-            { body, query, params }
-        );
+        const {
+            body: { payload },
+            query,
+            params,
+        } = req;
+        console.log('🚀 GET', { payload, query, params });
         res.status(200).json({ body, query, params });
     };
     PostIndex = (req, res) => {
-        const { body, query, params } = req;
-        console.log(
-            '🚀 ~ file: web-hook.js ~ line 12 ~ WebHookController ~ { body, query, params }',
-            { body, query, params }
-        );
+        const {
+            body: { payload },
+            query,
+            params,
+        } = req;
+        console.log('🚀 POST', { payload, query, params });
         res.status(200).json({ body, query, params });
     };
 }
