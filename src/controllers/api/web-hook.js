335a5bf4422968e45e01ef62ee859f28cf9dea2c
tiencv <tiencv@fiisoft.com>
2022-03-31 10:44:37 +0700

    update: add logs

--- src/controllers/api/web-hook.js
+++ src/controllers/api/web-hook.js
@@ -5,10 +5,14 @@ const HooksModel = require('../../models/hooks');
 class WebHookController {
     GetIndex = async (req, res) => {
         let { web_hook_payload = false } = req.body?.payload;
+        console.log(
+            '🚀 ~ file: web-hook.js ~ line 8 ~ WebHookController ~ GetIndex= ~ web_hook_payload',
+            web_hook_payload[0]
+        );
 
-        if (web_hook_payload[0].id) {
-            web_hook_payload = web_hook_payload.shift().web_hook_payload;
-        }
+        // if (web_hook_payload[0]?.id) {
+        //     web_hook_payload = web_hook_payload.shift().web_hook_payload;
+        // }
         if (!req.body?.payload?.web_hook_payload) {
             return res.status(400).end();
         }
