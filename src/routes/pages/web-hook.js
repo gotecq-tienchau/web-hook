6443d04c38f161a79a05a153107ff391693645fd
tiencv <tiencv@fiisoft.com>
2022-03-31 10:09:49 +0700

    update: add readme

--- src/routes/pages/web-hook.js
+++ src/routes/pages/web-hook.js
@@ -3,6 +3,5 @@ const Router = express.Router();
 const WebHookController = require('../../controllers/page/web-hook');
 
 Router.get('/', WebHookController.GetIndex);
-// Router.post('/add-hook', WebHookController.PostIndex);
 
 module.exports = Router;
